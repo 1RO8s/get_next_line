e8e0053b7c534cdec11e7f24bf76772a53240882
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-28 21:21:11 +0900

    add helper function in utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,7 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 19:12:36 by hnagasak          #+#    #+#             */
+/*   Updated: 2023/05/28 19:33:30 by hnagasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
 
-// 改行まで
-char* (char *buf)
+size_t	ft_strlen(const char *s)
 {
-    
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	srclen;
+
+	srclen = ft_strlen(src);
+	i = 0;
+	if (dstsize <= 0)
+		return (srclen);
+	while (i + 1 < dstsize && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (srclen);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+
+	s2 = ft_calloc(ft_strlen(s1) + 1, sizeof(char));
+	if (s2 != NULL)
+		ft_strlcpy(s2, s1, ft_strlen(s1) + 1);
+	return (s2);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	char	*start;
+	size_t	i;
+	size_t	dstlen;
+
+	if (dst == NULL && dstsize != 0)
+		return (ft_strlen(NULL));
+	if (dst == NULL)
+		dstlen = 0;
+	else
+		dstlen = ft_strlen(dst);
+	start = dst;
+	while (start != NULL && *start != '\0')
+		start++;
+	i = 0;
+	while (dstlen + i + 1 < dstsize && src[i] != '\0')
+	{
+		start[i] = src[i];
+		i++;
+	}
+	if (dst != NULL)
+		start[i] = '\0';
+	if (dstlen < dstsize)
+		return (dstlen + ft_strlen(src));
+	else
+		return (dstsize + ft_strlen(src));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	size_t	len;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	else if (s1 == NULL && s2 != NULL)
+		return (ft_strdup(s2));
+	else if (s1 != NULL && s2 == NULL)
+		return (ft_strdup(s1));
+	len = ft_strlen(s1) + ft_strlen(s2);
+	s3 = ft_calloc(len + 1, sizeof(char));
+	if (s3 != NULL)
+	{
+		ft_strlcpy(s3, s1, len + 1);
+		ft_strlcat(s3, s2, len + 1);
+	}
+	return (s3);
 }

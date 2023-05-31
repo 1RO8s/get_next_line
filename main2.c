e7f7ed3a7d7b797936f3cfd9466d4e1534de5af8
hnagasak <hnagasak@student.42tokyo.jp>
2023-05-31 21:07:39 +0900

    update test code

--- main2.c
+++ main2.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 
 void sample1(){
 	int		fd;
@@ -20,24 +20,25 @@ void sample1(){
 		printf("malloc fail\n");
 
 	read_bytes = read(fd, buf, BUFFER_SIZE);
-	buf[read_bytes] = '\0';
-    printf("buf1: read_bytes=%ld %s\n",read_bytes,buf);
+	// buf[read_bytes] = '\0';
+    printf("buf1: read_bytes=%ld strlen(buf)=%ld BUFFER_SIZE:%d\n",read_bytes,strlen(buf),BUFFER_SIZE);
+	printf("line:%s\n",buf);
 
 	read_bytes = read(fd, buf, BUFFER_SIZE);
-	buf[read_bytes] = '\0';
-    printf("buf2: read_bytes=%ld %s\n",read_bytes,buf);
+	// buf[read_bytes] = '\0';
+    printf("buf2: read_bytes=%ld strlen(buf)=%ld \nline:%s\n",read_bytes,strlen(buf),buf);
 	
 	read_bytes = read(fd, buf, BUFFER_SIZE);
 	buf[read_bytes] = '\0';
-    printf("buf3: read_bytes=%ld %s\n",read_bytes,buf);
+    printf("buf3: read_bytes=%ld strlen(buf)=%ld \nline:%s\n",read_bytes,strlen(buf),buf);
 
 	read_bytes = read(fd, buf, BUFFER_SIZE);
 	buf[read_bytes] = '\0';
-    printf("buf4: read_bytes=%ld %s\n",read_bytes,buf);
+    printf("buf4: read_bytes=%ld strlen(buf)=%ld \nline:%s\n",read_bytes,strlen(buf),buf);
 
 	read_bytes = read(fd, buf, BUFFER_SIZE);
 	buf[read_bytes] = '\0';
-    printf("buf5: read_bytes=%ld %s\n",read_bytes,buf);
+    printf("buf5: read_bytes=%ld strlen(buf)=%ld \nline:%s\n",read_bytes,strlen(buf),buf);
 }
 
 int	main(void)
--- test.c
+++ test.c
@@ -6,27 +6,65 @@
 /*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:33:18 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/05/27 16:12:58 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/05/31 19:11:49 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int main(){
-    int fd = open("./testfiles/file002",O_RDONLY);
-    printf("fd:%d\n",fd);
+void read_single_file()
+{
+    int fd = open("./testfiles/file002", O_RDONLY);
+    printf("fd:%d\n", fd);
 
-    char *line = (char*)malloc(sizeof(char) * BUFFER_SIZE);
+    char *line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
     if (line == NULL)
         printf("fail allocate memories\n");
 
-    // for(int i=0;i<10;i++){
+    for (int i = 0; i < 5; i++)
+    {
         line = get_next_line(fd);
-        printf("line:%s",line);
-    // }
+        printf("line %d:%s", i + 1, line);
+    }
+    close(fd);
 }
 
+void read_multiple_file()
+{
+    int fd[3];
 
+    char *file_paths[3] = {
+        "./testfiles/file001",
+        "./testfiles/file002",
+        "./testfiles/file003"};
 
+    // 複数ファイルopen
+    for (int i = 0; i < 3; i++)
+    {
+        fd[i] = open(file_paths[i], O_RDONLY);
+        printf("fd[%d]:%d\n", i, fd[i]);
+    }
 
+    for (int j = 0; j < 2; j++)
+    {
+        for (int i = 0; i < 3 ;i++)
+        {
+            char *line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+            if (line == NULL)
+                printf("fail allocate memories\n");
+            line = get_next_line(fd[i]);
+            printf("fd[%d] line %d:%s", i, j + 1, line);
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        close(fd[i]);
+    }
+}
+
+int main()
+{
+    read_multiple_file();
+}

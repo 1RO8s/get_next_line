0e2611ffd924816867aa566366b9613542c496be
hnagasak <hnagasak@student.42tokyo.jp>
2023-06-21 17:59:23 +0900

    fix header protection at bonus header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 14:20:10 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/06/21 17:51:11 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/06/21 17:58:26 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42

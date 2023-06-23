37bbc49faacfdb6459e5a748372d1d0be04b065c
hnagasak <hnagasak@student.42tokyo.jp>
2023-06-23 21:17:57 +0900

    add free function for error handling

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 17:53:30 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/06/23 16:43:44 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/06/23 21:05:38 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,10 @@ void	ft_bzero(void *s, size_t n)
  * Keep reading until a newline or the end of the line.
  * Returns NULL if reading fails.
  */
-static char	*ft_read_to_newline(int fd, char *read_str)
+static char	*ft_read_to_newline(int fd, char *read_str_old)
 {
 	char	*buf;
-	char	*read_str_old;
+	char	*read_str;
 	ssize_t	read_bytes;
 
 	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
@@ -66,9 +66,11 @@ static char	*ft_read_to_newline(int fd, char *read_str)
 		if (read_bytes == -1)
 			return (free_return(buf, NULL));
 		buf[read_bytes] = '\0';
-		read_str_old = read_str;
 		read_str = ft_strjoin(read_str_old, buf);
 		free(read_str_old);
+		if (read_str == NULL)
+			return (free_return(buf, NULL));
+		read_str_old = read_str;
 	}
 	free(buf);
 	if (read_str[0] == 0)
@@ -78,29 +80,24 @@ static char	*ft_read_to_newline(int fd, char *read_str)
 
 char	*get_next_line(int fd)
 {
-	static char	*prev[MAX_FD];
-	char		*read_str;
+	static char	*save[MAX_FD];
 	char		*line;
-	char		*newline;
+	char		*line_break_p;
 
 	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
 		return (0);
-	read_str = ft_read_to_newline(fd, prev[fd]);
-	if (!read_str)
-		return (NULL);
-	line = ft_strdup(read_str);
+	line = ft_read_to_newline(fd, save[fd]);
 	if (line == NULL)
-		return (free_return(line, NULL));
-	free(read_str);
-	newline = ft_strchr(line, '\n');
-	if (newline != NULL)
+		return (NULL);
+	line_break_p = ft_strchr(line, '\n');
+	if (line_break_p != NULL)
 	{
-		prev[fd] = ft_strdup(newline + 1);
-		if (prev[fd] == NULL)
+		save[fd] = ft_strdup(line_break_p + 1);
+		if (save[fd] == NULL)
 			return (free_return(line, NULL));
-		*(newline + 1) = '\0';
+		*(line_break_p + 1) = '\0';
 	}
 	else
-		prev[fd] = NULL;
+		save[fd] = NULL;
 	return (line);
 }
